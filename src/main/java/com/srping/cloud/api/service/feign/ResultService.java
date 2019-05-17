1e4ebecef0987c95e7c5f6e29ea7329c2d758298
wangyongst <wangyongst@gmail.com>
2019-05-17 16:17:08 +0800

    create

--- src/main/java/com/srping/cloud/api/service/feign/ResultService.java
+++ src/main/java/com/srping/cloud/api/service/feign/ResultService.java
@@ -9,22 +9,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(value = "spring-cloud-utils")
 public interface ResultService {
 
-    @GetMapping(value = "result/ok")
-    Result ok();
-
     @GetMapping(value = "result/okWithMessage")
     Result okWithMessage(@RequestParam("message") String message);
 
-    @GetMapping(value = "result/okWithData")
-    Result okWithData(@RequestParam("data") Object data);
-
-    @GetMapping(value = "result/okWithDataAndMessage")
-    Result okWithDataAndMessage(@RequestParam("data")  Object data, @RequestParam("message")  String message);
-
     @GetMapping(value = "result/parameterNotEnoughWithDataAndMessage")
     Result parameterNotEnoughWithDataAndMessage(@RequestParam("data")  Object data, @RequestParam("message")  String message);
-
-    @GetMapping(value = "result/parameterErrorWithDataAndMessage")
-    Result parameterErrorWithDataAndMessage(@RequestParam("data")  Object data, @RequestParam ("message") String message);
-
 }
