334da28149600393a2faf4d909feeb4f6e2ed19e
wangyongst <wangyongst@gmail.com>
2019-06-10 19:10:28 +0800

    home

--- src/main/java/com/srping/cloud/api/service/feign/UserService.java
+++ src/main/java/com/srping/cloud/api/service/feign/UserService.java
@@ -1,12 +1,12 @@
 package com.srping.cloud.api.service.feign;
+
 import com.srping.cloud.api.model.User;
-import com.srping.cloud.api.service.feign.hystric.UserServiceHystric;
 import com.srping.cloud.api.utils.Result;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(value = "spring-cloud-user-service",fallback = UserServiceHystric.class)
+@FeignClient(value = "spring-cloud-user-service")
 public interface UserService {
 
     @PostMapping(value = "/user/regist")
